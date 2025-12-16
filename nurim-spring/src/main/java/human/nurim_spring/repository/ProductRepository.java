7c9a53b3cb923e24c4152f54858ca9a423e21424
LSM <qdpnok@naver.com>
2025-12-16 14:15:48 +0900

    상품 조회 테스트

---
+++ nurim-spring/src/main/java/human/nurim_spring/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package human.nurim_spring.repository;
+
+import human.nurim_spring.entity.Product;
+import human.nurim_spring.entity.SubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findBySubCategory(SubCategory subCategory);
+}
