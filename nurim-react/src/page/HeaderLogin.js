e26ad043cd3fda8e19090ae2f3643063264c0c5e
alphalogis <p94016775@gmail.com>
2025-12-18 17:32:41 +0900

    메인 이미지 추가됨.

--- nurim-react/src/page/HeaderLogin.js
+++ nurim-react/src/page/HeaderLogin.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 // import BellIconImg from "../img/bell.png";
 // import CartIconImg from "../img/cart.png";
 // import ProfileImg from "../img/profile.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom"; // useNavigate 추가
 import MainLogo from "../img/MainLogo.png";
 import SearchIcon from "../img/search.png";
 
@@ -121,11 +121,33 @@ const CartBadge = styled(BadgeBase)`
   background-color: #00cc82; /* 초록색 */
 `;
 
-// 4. 프로필 이미지 (왼쪽 1283px)
+// --- 로그아웃 버튼 스타일 추가 ---
+const LogoutButton = styled.button`
+  position: absolute;
+  left: 1280px; /* 아이콘들 오른쪽 적절한 위치에 배치 */
+  top: 25px;
+  background: none;
+  border: 1px solid #d9d9d9;
+  border-radius: 5px;
+  padding: 5px 15px;
+  cursor: pointer;
+  font-family: "Outfit-Regular", Helvetica, sans-serif;
+  font-size: 12px;
+  color: #1e1e1e;
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: #1e1e1e;
+    color: #ffffff;
+    border-color: #1e1e1e;
+  }
+`;
+
+// 4. 프로필 이미지 (왼쪽 1283px -> 로그아웃 버튼 때문에 위치 조정 필요 시 수정)
 const ProfileWrapper = styled.div`
   height: 44px;
   width: 44px;
-  left: 1283px;
+  left: 1360px; /* 로그아웃 버튼 옆으로 밀어둠 (예시) */
   position: absolute;
   top: 17px; /* 중앙 정렬을 위해 위치 조정 */
   border-radius: 22px;
@@ -194,6 +216,21 @@ const SearchImg = styled.img`
 
 export const HeaderLogin = () => {
   const [search, setSearch] = useState("");
+  const nav = useNavigate(); // 페이지 이동을 위한 훅
+
+  // 로그아웃 핸들러
+  const handleLogout = () => {
+    // 1. 사용자 확인 (선택 사항)
+    if (!window.confirm("로그아웃 하시겠습니까?")) return;
+
+    // 2. 토큰 및 사용자 정보 삭제 (저장 방식에 따라 키 이름 변경 필요)
+    // 예: localStorage, sessionStorage, Cookies
+    localStorage.removeItem("accessToken");
+
+    // 3. 로그아웃 완료 알림 및 페이지 이동
+    alert("로그아웃 되었습니다.");
+    window.location.href = "/";
+  };
 
   return (
     <Container>
@@ -211,7 +248,7 @@ export const HeaderLogin = () => {
         </SearchGroup>
 
         <Link to="/">
-          <Logo src={MainLogo} alt="asd" />
+          <Logo src={MainLogo} alt="Logo" />
         </Link>
 
         {/* 1. 메시지 아이콘 (파란 뱃지 '0') */}
@@ -219,8 +256,7 @@ export const HeaderLogin = () => {
           {/* <img src={MessageIconImg} alt="msg" style={{width: '20px'}} /> */}
           <div
             style={{ width: "20px", height: "20px", background: "#ccc" }}
-          ></div>{" "}
-          {/* 임시 아이콘 */}
+          ></div>
           <MessageBadge>0</MessageBadge>
         </MessageWrapper>
 
@@ -229,9 +265,8 @@ export const HeaderLogin = () => {
           {/* <img src={BellIconImg} alt="bell" style={{width: '20px'}} /> */}
           <div
             style={{ width: "20px", height: "20px", background: "#ccc" }}
-          ></div>{" "}
-          {/* 임시 아이콘 */}
-          <BellBadge>99+</BellBadge>
+          ></div>
+          {/* <BellBadge>99+</BellBadge> */}
         </BellWrapper>
 
         {/* 3. 장바구니 아이콘 (초록 뱃지 '6') */}
@@ -239,16 +274,18 @@ export const HeaderLogin = () => {
           {/* <img src={CartIconImg} alt="cart" style={{width: '20px'}} /> */}
           <div
             style={{ width: "20px", height: "20px", background: "#ccc" }}
-          ></div>{" "}
-          {/* 임시 아이콘 */}
+          ></div>
           <CartBadge>6</CartBadge>
         </CartWrapper>
 
-        {/* 4. 프로필 이미지 */}
-        <ProfileWrapper>
-          {/* 이미지가 있으면 아래 주석 풀고 사용 */}
-          {/* <ProfileImage src={ProfileImg} alt="Profile" /> */}
-        </ProfileWrapper>
+        {/* 로그아웃 버튼 */}
+        <LogoutButton onClick={handleLogout}>Logout</LogoutButton>
+
+        {/* 프로필 이미지 (필요 시 주석 해제하여 사용) */}
+        {/* <ProfileWrapper>
+             <ProfileImage src={ProfileImg} alt="Profile" />
+           </ProfileWrapper> 
+        */}
       </HeaderWrapper>
 
       {/* 네비게이션 바 */}
