b9500101d2e99045517b863167c28cdc9682ddb7
LSM <qdpnok@naver.com>
2025-12-22 09:29:10 +0900

    MemberRepositoryTest 주석 추가

--- nurim-spring/src/main/java/human/nurim_spring/service/CartService.java
+++ nurim-spring/src/main/java/human/nurim_spring/service/CartService.java
@@ -1,5 +1,7 @@
 package human.nurim_spring.service;
 
+import human.nurim_spring.repository.CartItemRepository;
+import human.nurim_spring.repository.CartRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,5 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CartService {
-    // 
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    // 카트 아이템 추가
+
+    // 카트 내 아이템 조회
+    // 카트 내 아이템 전체 삭제
+    // 카트 아이템 선택 삭제
 }
