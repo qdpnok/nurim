ad6eec97270bd72f4baf4d44147ddee972d27918
LSM <qdpnok@naver.com>
2025-12-22 09:47:29 +0900

    AuthServiceTest 테스트 부활

--- nurim-spring/src/test/java/human/nurim_spring/service/AuthServiceTest.java
+++ nurim-spring/src/test/java/human/nurim_spring/service/AuthServiceTest.java
@@ -3,6 +3,7 @@ package human.nurim_spring.service;
 import human.nurim_spring.dto.LoginReqDto;
 import human.nurim_spring.dto.LoginResDto;
 import human.nurim_spring.dto.SignUpReqDto;
+import human.nurim_spring.dto.TokenDto;
 import human.nurim_spring.entity.Member;
 import human.nurim_spring.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -24,26 +25,27 @@ class AuthServiceTest {
     @Autowired
     AuthService authService;
 
-    // 회원 가입
-//    @Test
-//    @DisplayName("회원 가입, 로그인 테스트")
-//    public void signupTest() {
-//        SignUpReqDto dto = new SignUpReqDto();
-//        dto.setId("tkdal");
-//        dto.setPwd("pwd0000");
-//        dto.setEmail("tkdal@gmail.com");
-//        dto.setName("이상미");
-//        dto.setPhoneNum("010-1234-1234");
-//
-//        log.info("existById : {}", authService.existsById(dto.getId()));
-//        log.info("existByEmail: {}", authService.existsByEmail(dto.getEmail()));
-//
-//
-//        LoginReqDto loginReqDto = new LoginReqDto();
-//        loginReqDto.setId("tkdal");
-//        loginReqDto.setPwd("pwd0000");
-//        LoginResDto loginResDto = authService.login(loginReqDto);
-//        log.info("로그인 : {}", loginResDto);
-//    }
+    @Test
+    @DisplayName("회원 가입, 로그인 테스트")
+    public void signupTest() {
+        SignUpReqDto dto = new SignUpReqDto();
+        dto.setId("tkdal");
+        dto.setPwd("pwd0000");
+        dto.setEmail("tkdal@gmail.com");
+        dto.setName("이상미");
+        dto.setPhoneNum("010-1234-1234");
+
+        log.info("existById : {}", authService.existsById(dto.getId()));
+        log.info("existByEmail: {}", authService.existsByEmail(dto.getEmail()));
+
+        authService.signUp(dto);
+
+
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setId("tkdal");
+        loginReqDto.setPwd("pwd0000");
+        TokenDto tokenDto = authService.login(loginReqDto);
+        log.info("로그인 : {}", tokenDto.getAccessToken());
+    }
 
 }
