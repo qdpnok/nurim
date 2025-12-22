2375347618c1d5617ae122d432c0cd76ac45bb71
LSM <qdpnok@naver.com>
2025-12-22 12:05:53 +0900

    일단 카트 제외

--- nurim-spring/src/main/java/human/nurim_spring/entity/PurchaseCart.java
+++ nurim-spring/src/main/java/human/nurim_spring/entity/PurchaseCart.java
@@ -8,18 +8,18 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Getter @Setter @NoArgsConstructor
+//@Entity
+//@Getter @Setter @NoArgsConstructor
 public class PurchaseCart {
-    @Id
-    @Column(name = "purchase_cart_num")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long num;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_num")
-    private Member member;
-
-    @OneToMany(mappedBy = "purchase_cart", cascade = CascadeType.ALL)
-    private List<CartItem> cartItems = new ArrayList<>();
+//    @Id
+//    @Column(name = "purchase_cart_num")
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long num;
+//
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_num")
+//    private Member member;
+//
+//    @OneToMany(mappedBy = "purchase_cart", cascade = CascadeType.ALL)
+//    private List<CartItem> cartItems = new ArrayList<>();
 }
