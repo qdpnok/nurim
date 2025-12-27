918eb90dd5bf84ad84de90d1b2cb24c22d8fadb6
LSM <qdpnok@naver.com>
2025-12-27 17:04:53 +0900

    order 테이블 추가

---
+++ nurim-spring/src/main/java/human/nurim_spring/constant/OrderStatus.java
@@ -0,0 +1,5 @@
+package human.nurim_spring.constant;
+
+public enum OrderStatus {
+    ORDERED, CANCELED, COMPLETED
+}
---
+++ nurim-spring/src/main/java/human/nurim_spring/entity/Order.java
@@ -0,0 +1,40 @@
+package human.nurim_spring.entity;
+
+import human.nurim_spring.constant.OrderStatus;
+import human.nurim_spring.constant.OrderType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Order {
+    @Id
+    @Column(name = "order_num")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long num;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_num")
+    private Member member;
+
+    private String orderName;
+    private String orderEmail;
+    private String orderPhone;
+
+    @Enumerated(EnumType.STRING)
+    private OrderType orderType;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime canceledAt;
+
+    @PrePersist
+    public void prePersist() { this.createdAt = LocalDateTime.now(); }
+}
