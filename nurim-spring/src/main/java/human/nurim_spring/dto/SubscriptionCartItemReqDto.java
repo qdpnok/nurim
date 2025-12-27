3a3125d220fdc63b8d11398ace71208968ecb136
LSM <qdpnok@naver.com>
2025-12-27 12:54:11 +0900

    구독 장바구니 서비스 테스트 완료

--- nurim-spring/src/main/java/human/nurim_spring/dto/SubscriptionCartItemReqDto.java
+++ nurim-spring/src/main/java/human/nurim_spring/dto/SubscriptionCartItemReqDto.java
@@ -1,10 +1,12 @@
 package human.nurim_spring.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter @NoArgsConstructor
+@AllArgsConstructor
 public class SubscriptionCartItemReqDto {
     private Long memberNum;
     private Long productNum;
