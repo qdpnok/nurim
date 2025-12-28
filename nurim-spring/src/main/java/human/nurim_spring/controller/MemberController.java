15cb80d849b40476bf10d388a2a5c21c09ff9880
LSM <qdpnok@naver.com>
2025-12-29 04:52:58 +0900

    회원 정보 수정 컨트롤러

--- nurim-spring/src/main/java/human/nurim_spring/controller/MemberController.java
+++ nurim-spring/src/main/java/human/nurim_spring/controller/MemberController.java
@@ -1,13 +1,16 @@
 package human.nurim_spring.controller;
 
+import human.nurim_spring.dto.ChangeInfoReqDto;
 import human.nurim_spring.dto.MyInfoResDto;
 import human.nurim_spring.dto.MyPageResDto;
 import human.nurim_spring.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -26,5 +29,16 @@ public class MemberController {
         return ResponseEntity.ok(memberService.myInfo(memberNum));
     }
 
+    @PostMapping("/my-info/pwd/{memberNum}")
+    public ResponseEntity<Void> changePwd(@PathVariable Long memberNum, @RequestBody Map<String, String> body) {
+        String pwd = body.get("pwd");
+        memberService.changePwd(memberNum, pwd);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PostMapping("/my-info/info/{memberNum}")
+    public ResponseEntity<MyInfoResDto> changeInfo(@PathVariable Long memberNum, @RequestBody ChangeInfoReqDto dto) {
+        return ResponseEntity.ok(memberService.changeInfo(memberNum, dto));
+    }
 
 }
