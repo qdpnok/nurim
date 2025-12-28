0bcd35a3e583a057890eb61e9f9c2c4aad42019c
LSM <qdpnok@naver.com>
2025-12-29 03:10:00 +0900

    마이페이지 메인 렌더링

---
+++ nurim-spring/src/main/java/human/nurim_spring/dto/MyPageResDto.java
@@ -0,0 +1,13 @@
+package human.nurim_spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+@AllArgsConstructor
+public class MyPageResDto {
+    Long subscriptionCount;
+    Long purchaseCount;
+}
---
+++ nurim-spring/src/main/java/human/nurim_spring/service/MemberService.java
@@ -0,0 +1,32 @@
+package human.nurim_spring.service;
+
+import human.nurim_spring.dto.MyPageResDto;
+import human.nurim_spring.entity.Member;
+import human.nurim_spring.entity.Purchase;
+import human.nurim_spring.error.BusinessException;
+import human.nurim_spring.repository.MemberRepository;
+import human.nurim_spring.repository.PurchaseRepository;
+import human.nurim_spring.repository.SubscriptionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final PurchaseRepository purchaseRepository;
+    private final SubscriptionRepository subscriptionRepository;
+    private final MemberRepository memberRepository;
+
+    // 마이페이지 메인 렌더링
+    public MyPageResDto main(Long memberNum) {
+        Member member = memberRepository.findById(memberNum)
+                .orElseThrow(() -> new BusinessException("NOT_EXIST_MEMBER", "해당 회원이 존재하지 않습니다."));
+
+        Long purchaseCount = purchaseRepository.countByMember(member);
+        Long subscriptionCount = subscriptionRepository.countByMember(member);
+
+        return new MyPageResDto(subscriptionCount, purchaseCount);
+    }
+}
