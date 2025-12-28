0ddcd998e66ceadbab8d94a59900c425ff4376fe
LSM <qdpnok@naver.com>
2025-12-29 08:40:35 +0900

    반납 테이블에 구독 연관 관계 추가 인수 테이블 생성

--- nurim-spring/src/main/java/human/nurim_spring/entity/Acquisition.java
+++ nurim-spring/src/main/java/human/nurim_spring/entity/Acquisition.java
@@ -1,41 +1,32 @@
 package human.nurim_spring.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.security.Timestamp;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Entity
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
-@Builder
-@AllArgsConstructor
-public class Returns {
+@Getter @Setter @NoArgsConstructor
+public class Acquisition {
     @Id
-    @Column(name = "return_num")
+    @Column(name = "acquisition_num")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long num;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name ="member_num")
+    @JoinColumn(name = "member_num")
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_num")
     private Product product;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "subscription_num")
+    @OneToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="subscription_num")
     private Subscription subscription;
 
     @Column
-    private Long quantity;
-
-    @Column
-    private LocalDateTime return_data;
-
+    private LocalDateTime acquisition_data;
 }
